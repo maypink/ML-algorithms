c89eeb9670de549031bf1889b2e12c16576b49b2
Pinchuk Maya <mayaprr@yandex.ru>
2021-12-29 02:56:26 +0300

    fix types and inheritance

--- knn/k_neighbors_classifier.py
+++ knn/k_neighbors_classifier.py
@@ -7,25 +7,29 @@ class KNeighborsClassifier:
     def __init__(self, k: int = 3):
         self.k = k
 
-    def dist(self, sample: np.ndarray, cur_sample: np.ndarray) -> float:
+    def calc_dist(self, sample: np.ndarray, cur_sample: np.ndarray) -> float:
         euclidian_dist = 0
         for i in range(len(sample) - 1):
             euclidian_dist += (sample[i] - cur_sample[i]) ** 2
         euclidian_dict = euclidian_dist ** 0.5
         return euclidian_dist
 
-    def _predict(self, sample: np.ndarray, X: np.ndarray, y: np.ndarray) -> int:
+    def get_sorted_distances(self, sample: np.ndarray, x: np.ndarray):
         samples_dist = {}
-        for i in range(len(X) - 1):
-            cur_dist = self.dist(sample, X[i])
+        for i in range(len(x) - 1):
+            cur_dist = self.calc_dist(sample, x[i])
             samples_dist[cur_dist] = i
-        sorted_dist = sorted(samples_dist.items(), key=lambda x: x[0])
-        idxs = list(i[1] for i in sorted_dist[:(self.k)])
+        sorted_dist = sorted(samples_dist.items(), key=lambda q: q[0])
+        return sorted_dist
+
+    def _predict_for_one(self, sample: np.ndarray, x: np.ndarray, y: np.ndarray) -> int:
+        sorted_dist = self.get_sorted_distances(sample, x)
+        idxs = list(i[1] for i in sorted_dist[:self.k])
         neighbors = list(y[i] for i in idxs)
         return max(set(neighbors), key=neighbors.count)
 
-    def predict(self, samples: np.ndarray, X: np.ndarray, y: np.ndarray) -> List[int]:
+    def predict(self, samples: np.ndarray, x: np.ndarray, y: np.ndarray) -> List[int]:
         preds = []
         for sample in tqdm(samples):
-            preds.append(self._predict(sample, X, y))
+            preds.append(self._predict_for_one(sample, x, y))
         return preds
--- knn/k_neighbours_regression.py
+++ knn/k_neighbours_regression.py
@@ -1,44 +1,33 @@
 from tqdm import tqdm
 import numpy as np
 from typing import List
+from k_neighbors_classifier import KNeighborsClassifier
 
 
-class KNeighborsRegression:
-    def __init__(self, k: int = 3):
-        self.k = k
+class KNeighborsRegression(KNeighborsClassifier):
 
-    def dist(self, sample: np.ndarray, cur_sample: np.ndarray) -> float:
-        euclidian_dist = 0
-        for i in range(len(sample) - 1):
-            euclidian_dist += (sample[i] - cur_sample[i]) ** 2
-        euclidian_dict = euclidian_dist ** 0.5
-        return euclidian_dist
-
-    def weighted_average(self, sorted_dist: List[float], y: np.ndarray) -> float:
+    @staticmethod
+    def weighted_average(sorted_dist: List[float], y: np.ndarray) -> float:
         num = 0
         for (dist, idx) in sorted_dist:
             num += dist ** (-1) * y[idx]
         denom = sum(list(dist ** (-1) for dist, _ in sorted_dist))
         return num / denom
 
-    def _predict(self, sample: np.ndarray, X: np.ndarray, y: np.ndarray) -> int:
-        samples_dist = {}
-        for i in range(len(X) - 1):
-            cur_dist = self.dist(sample, X[i])
-            samples_dist[cur_dist] = i
-        sorted_dist = sorted(samples_dist.items(), key=lambda x: x[0])
+    def _predict_for_one(self, sample: np.ndarray, x: np.ndarray, y: np.ndarray) -> float:
+        sorted_dist = self.get_sorted_distances(sample, x)
         pred = self.weighted_average(sorted_dist[:self.k], y)
         return pred
 
-    def predict_proba(self, samples: np.ndarray, X: np.ndarray, y: np.ndarray) -> List[int]:
+    def predict_proba(self, samples: np.ndarray, x: np.ndarray, y: np.ndarray) -> List[float]:
         preds = []
         for sample in samples:
-            preds.append(self._predict(sample, X, y))
+            preds.append(self._predict_for_one(sample, x, y))
         return preds
 
-    def predict(self, samples: np.ndarray, X: np.ndarray, y: np.ndarray, th: float = 0.5) -> List[int]:
+    def predict(self, samples: np.ndarray, x: np.ndarray, y: np.ndarray, th: float = 0.5) -> List[bool]:
         preds = []
         for sample in tqdm(samples):
-            pred = (self._predict(sample, X, y) > th)
+            pred = (self._predict_for_one(sample, x, y) > th)
             preds.append(pred)
         return preds
